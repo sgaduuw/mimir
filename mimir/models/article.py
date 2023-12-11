5b89d21917780ebe3787cd3c820da44bbbdd8c33
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2023-12-11 21:30:17 +0100

    Store server id for article

--- mimir/models/article.py
+++ mimir/models/article.py
@@ -7,4 +7,5 @@ class Article(db.Document):
     author = db.StringField()
     references = db.StringField()
     message_id = db.StringField(unique=True)
+    srv_id = db.IntField(unique=True)
     date = db.DateTimeField(default=datetime.utcnow)
