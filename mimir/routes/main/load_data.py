ddbc5277404b1d171ea116b936b43be45e73ae5f
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2023-12-21 08:15:16 +0100

    Handle encodings slightly better, this helps consuming articles from NNTP that have non-ASCII encodings

--- mimir/routes/main/load_data.py
+++ mimir/routes/main/load_data.py
@@ -1,6 +1,7 @@
 """ this is a placeholder route to easily force an import 
     it will be moved to a celery task later
 """
+import chardet
 import nntplib
 import re
 from datetime import datetime
@@ -15,12 +16,21 @@ nntp_group = 'org.kernel.vger.linux-kernel'
 
 s = nntplib.NNTP(nntp_server)
 
+def detect_encoding(input):
+    result = chardet.detect(input)
+    encoding = result['encoding']
+    print(f"ENCODING: {encoding}")
+    return encoding
+
 def process_plain_text(msg, meta: dict):
     # get the body-less metadata, add body key to it
     # eventually this will probably need to do more
     full_article = meta
-    full_article['body'] = msg.get_payload(decode=True).decode()
+    article_body = msg.get_payload(decode=True)
+    encoding = detect_encoding(article_body)
+    full_article['body'] = article_body.decode(encoding)
     return full_article
+
 
 def get_attachment_filename(input: str):
     if input:
@@ -31,22 +41,38 @@ def get_attachment_filename(input: str):
 
     return None
 
+
 def process_mime_message(msg, meta: dict):
+    full_article = meta
     attachments = []
     for part in msg.walk():
         content_type = part.get_content_type()
         content_disposition = str(part.get("Content-Disposition"))
 
         print(f"MIME: {content_type} - {content_disposition}")
-        if content_type == 'multipart/mixed':
+        skipped_types = [
+            'application/x-gzip',
+            'application/x-tar-gz',
+            'application/octet-stream',
+            'multipart/mixed',
+            'application/x-gunzip'
+        ]
+        if content_type in skipped_types:
             continue
 
-        if "attachment" not in content_disposition:
-            body = part.get_payload(decode=True).decode()
-            print(f"MIME BODY: {body}")
+        if 'attachment' not in content_disposition:
+            body = part.get_payload(decode=True)
+            if body:
+                encoding = detect_encoding(body)
+                body = body.decode(encoding)
+            else:
+                body = None
 
         else:
-            attachment_data = part.get_payload(decode=True).decode()
+            attachment_data = part.get_payload(decode=True)
+            attachment_encoding = detect_encoding(attachment_data)
+            attachment_data = attachment_data.decode(attachment_encoding)
+
             attachment_header, _ = decode_header(content_disposition)[0]
             filename = get_attachment_filename(attachment_header)
 
@@ -56,11 +82,13 @@ def process_mime_message(msg, meta: dict):
             }
             attachments.append(data)
 
-    # print(f"MIME: {content} | {meta}")
-    article = body
-    return {'article': article, 'attachments': attachments}
+    full_article['body'] = body
+    full_article['attachments'] = save_attachments(attachments_data=attachments, message_id=full_article['message_id'])
+    return full_article
+
 
 def save_article(n_article: dict):
+    print(f"SAVE: {n_article.keys()}")
     article = Article.objects(message_id=n_article['message_id']).first()
 
     if article is not None:
@@ -71,40 +99,39 @@ def save_article(n_article: dict):
 
     return article
 
-def save_attachments(attachments_data):
+
+def save_attachments(attachments_data: list, message_id: str):
     attachments = []
 
     for data in attachments_data:
-        print(f"ATT DATA: {data}")
         d_dict = {
-            'filename': None,
-            'content': None
+            'filename': data['filename'],
+            'content': data['data'],
+            'message_id': message_id
         }
         attachment = Attachment(**d_dict).save()
         attachments.append(attachment)
     return attachments
+
 
 def process_and_save_message(msg: str, meta: dict):
     message = message_from_string(msg)
 
     # Determine message type and process accordingly
     if message.is_multipart():
-        print("PROCESS: MULTIPART!")
-        article_data, attachment_data = process_mime_message(msg=message, meta=meta)
-        attachments = save_attachments(attachment_data)
-        article_data['attachment'] = attachments
+        article = process_mime_message(msg=message, meta=meta)
 
     else:
-        article_data = process_plain_text(msg=message, meta=meta)
+        article = process_plain_text(msg=message, meta=meta)
 
     # Save or update the Article model
-    save_article(article_data)
+    save_article(article)
 
 
 @bp_main.route('/import/')
 def import_page() -> str:
     resp, count, first, last, name = s.group(nntp_group)
-    resp, overviews = s.over((first, first + 50))
+    resp, overviews = s.over((first, first + 1000))
 
     for art_id, over in overviews:
         print(f"================= {art_id} ====================")
@@ -123,8 +150,10 @@ def import_page() -> str:
         else:
             resp, number, message_id = s.stat(art_id)
             resp, article = s.article(message_id)
+
             article_bin = b'\n'.join(article.lines)
-            article_dec = article_bin.decode()
+            article_encoding = detect_encoding(article_bin)
+            article_dec = article_bin.decode(encoding=article_encoding)
 
             article_meta = {
                 'author': mail_from,
